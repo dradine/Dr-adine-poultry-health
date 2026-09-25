c5fc8eb8617dd13c1933240accfea7ea4d1eddbd
dradine <dradine.vet@gmail.com>
2026-09-26 00:06:37 +0330

    Fix PI regression fixture to use canonical target contract

--- tests/broiler-performance-intelligence-v2.test.js
+++ tests/broiler-performance-intelligence-v2.test.js
@@ -5,7 +5,7 @@ const E=sandbox.AdineBroilerPerformanceIntelligenceV2;
 assert(E&&E.version==='BROILER-PI-V6.4');
 
 const base={standardWeight:1000,standardWeeklyFcr:1.30,standardCumulativeFcr:1.30,standardWeeklyWeightGain:60,standardMortalityPercent:1,standardCv:10,standardUniformity10:80,standardUniformity15:90,standardFeed:100,standardWater:180,standardWaterFeedRatio:1.8,standardEpef:400};
-function row(x){return Object.assign({age:35,week:5,weight:1000,fcr:1.30,cumulativeFcr:1.30,weeklyWeightGain:60,mortalityPercent:1,cv:10,uniformity10:80,uniformity15:90,feed:100,water:180,waterFeedRatio:1.8,epef:400},base,x)}
+function row(x){const d=Object.assign({age:35,week:5,weight:1000,fcr:1.30,cumulativeFcr:1.30,weeklyWeightGain:60,mortalityPercent:1,cv:10,uniformity10:80,uniformity15:90,feed:100,water:180,waterFeedRatio:1.8,epef:400},base,x); return Object.assign(d,{canonicalTargets:{weight:d.standardWeight,fcr:d.standardWeeklyFcr,cumulativeFcr:d.standardCumulativeFcr,adg:d.standardWeeklyWeightGain,mortality:d.standardMortalityPercent,cv:d.standardCv,u10:d.standardUniformity10,u15:d.standardUniformity15,feed:d.standardFeed,water:d.standardWater,wfr:d.standardWaterFeedRatio,epef:d.standardEpef}},x)}
 
 let m=E.build({genetics:'Ross',strain:'Ross 308 AP'},[row({}),row({})]);
 assert.strictEqual(m.status,'good');
